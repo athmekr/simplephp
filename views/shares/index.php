f3f0ad05f8271fa8e4f38542ce892c0a98b78512
athmekr <athmekr@gmail.com>
2019-06-06 01:05:03 +0300

    Added protection so you cannot share and add while not logged in

--- views/shares/index.php
+++ views/shares/index.php
@@ -1,6 +1,8 @@
 <div>
+    <?php if(isset($_SESSION['is_logged_in'])) : ?>
 	<a class="btn btn-success btn-share" href="<?php echo ROOT_PATH; ?>shares/add">Share Something</a>
-	<?php foreach($viewmodel as $item) : ?>
+    <?php endif; ?>
+    <?php foreach($viewmodel as $item) : ?>
 		<div class="well">
 			<h3><?php echo $item['title']; ?></h3>
 			<small><?php echo $item['create_date']; ?></small>
