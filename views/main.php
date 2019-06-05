6d0a7c6b47126e70c00cce91f779bc3add3bf614
athmekr <athmekr@gmail.com>
2019-06-06 00:56:38 +0300

    Edited main view

--- views/main.php
+++ views/main.php
@@ -1,8 +1,8 @@
 <html>
 <head>
 	<title>Shareboard</title>
-	<link rel="stylesheet" href="assets/css/bootstrap.css">
-	<link rel="stylesheet" href="assets/css/style.css">
+	<link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/css/bootstrap.css">
+	<link rel="stylesheet" href="<?php echo ROOT_PATH; ?>assets/css/style.css">
 </head>
 <body>
 	<nav class="navbar navbar-default">
@@ -23,8 +23,13 @@
           </ul>
 
           <ul class="nav navbar-nav navbar-right">
+            <?php if(isset($_SESSION['is_logged_in'])) : ?>
+            <li><a href="<?php echo ROOT_URL; ?>">Welcome <?php echo $_SESSION['user_data']['name']; ?></a></li>
+            <li><a href="<?php echo ROOT_URL; ?>users/logout">Logout</a></li>
+          <?php else : ?>
             <li><a href="<?php echo ROOT_URL; ?>users/login">Login</a></li>
             <li><a href="<?php echo ROOT_URL; ?>users/register">Register</a></li>
+          <?php endif; ?>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
